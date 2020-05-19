c3816bd95a7536fabb62f08bd72cb5f693d4f5bf
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-19 12:08:59 +0200

    168. PlayerPrefs To Save Settings

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -23,6 +23,20 @@ public class LevelLoader : MonoBehaviour
         yield return new WaitForSeconds(timeToWait);
         LoadNextScene();
     }
+    public void RestartScene()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Start Screen");
+    }
+    public void LoadOptionsScreen()
+    {
+        SceneManager.LoadScene("Options Screen");
+    }
 
     public void LoadNextScene()
     {
@@ -32,5 +46,10 @@ public class LevelLoader : MonoBehaviour
     public void LoadYouLose()
     {
         SceneManager.LoadScene("Lose Screen");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
